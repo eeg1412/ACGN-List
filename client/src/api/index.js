5b9f71b384efc67908d18087b9ce3da2e9cba6d4
eeg1412 <eeg1412@gmail.com>
2020-11-23 22:08:03 +0900

    验证

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,12 @@
 import { createAPI } from './create-api'
 import auth from './module/auth'
+// import { createBrowserHistory as createHistory } from "history";
 
 const api = createAPI({ baseURL: '/api' })
+
+// setTimeout(() => {
+//     const history = createHistory();
+//     history.replace('/anime')
+// }, 5000)
 
 export const authApi = auth(api)
--- client/src/view/admin/index.js
+++ client/src/view/admin/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Layout, Menu } from 'antd';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux'
 
 import series from './series'
@@ -26,10 +26,6 @@ class adminIndex extends Component {
         this.setState({
             current: path
         });
-        // 判断当前是否有登录信息
-        if (!this.props.adminToken) {
-            this.props.history.replace('/admin/login');
-        }
     }
     componentDidUpdate (prevProps, prevState) {
         if (prevProps.location.pathname !== this.props.location.pathname) {
@@ -46,43 +42,45 @@ class adminIndex extends Component {
     };
     render () {
         return (
-            <div className="acgnlist_admin_body">
-                <Layout>
-                    <Header>动画漫画游戏小说后台管理系统</Header>
+            <>
+                {this.props.adminToken ? (<div className="acgnlist_admin_body">
                     <Layout>
-                        <Sider
-                            breakpoint="lg"
-                            collapsedWidth="0">
-                            <div className="acgnlist_admin_menu_body">
-                                <Menu
-                                    onClick={this.handleClick}
-                                    selectedKeys={[this.state.current]}
-                                    mode="inline"
-                                >
-                                    <Menu.Item key="/admin/series">系列</Menu.Item>
-                                    <Menu.Item key="/admin/anime">动画</Menu.Item>
-                                    <Menu.Item key="/admin/comic">漫画</Menu.Item>
-                                    <Menu.Item key="/admin/game">游戏</Menu.Item>
-                                    <Menu.Item key="/admin/novel">小说</Menu.Item>
-                                </Menu>
-                            </div>
-                        </Sider>
-                        <Content className="p20">
-                            <Switch>
-                                <Route path="/admin/series" exact component={series}></Route>
-                                <Route path="/admin/anime" exact component={anime}></Route>
-                                <Route path="/admin/comic" exact component={comic}></Route>
-                                <Route path="/admin/game" exact component={game}></Route>
-                                <Route path="/admin/novel" exact component={novel}></Route>
-                            </Switch>
-                        </Content>
+                        <Header>动画漫画游戏小说后台管理系统</Header>
+                        <Layout>
+                            <Sider
+                                breakpoint="lg"
+                                collapsedWidth="0">
+                                <div className="acgnlist_admin_menu_body">
+                                    <Menu
+                                        onClick={this.handleClick}
+                                        selectedKeys={[this.state.current]}
+                                        mode="inline"
+                                    >
+                                        <Menu.Item key="/admin/series">系列</Menu.Item>
+                                        <Menu.Item key="/admin/anime">动画</Menu.Item>
+                                        <Menu.Item key="/admin/comic">漫画</Menu.Item>
+                                        <Menu.Item key="/admin/game">游戏</Menu.Item>
+                                        <Menu.Item key="/admin/novel">小说</Menu.Item>
+                                    </Menu>
+                                </div>
+                            </Sider>
+                            <Content className="p20">
+                                <Switch>
+                                    <Route path="/admin/series" exact component={series}></Route>
+                                    <Route path="/admin/anime" exact component={anime}></Route>
+                                    <Route path="/admin/comic" exact component={comic}></Route>
+                                    <Route path="/admin/game" exact component={game}></Route>
+                                    <Route path="/admin/novel" exact component={novel}></Route>
+                                </Switch>
+                            </Content>
+                        </Layout>
+                        <Footer>
+                            <div className="tc">Powered by <a href="https://www.wikimoe.com" target="_blank">wikimoe</a></div>
+                        </Footer>
                     </Layout>
-                    <Footer>
-                        <div className="tc">Powered by <a href="https://www.wikimoe.com" target="_blank">wikimoe</a></div>
-                    </Footer>
-                </Layout>
 
-            </div>
+                </div>) : (<Redirect to="/admin/login" />)}
+            </>
         );
     }
 }
